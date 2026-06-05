a48777a7394728d2b3e29be022d482a23c4b1e22
firmenauftritt <yunuskivilcim774@gmail.com>
2026-06-06 01:27:05 +0200

    Update kontakt.tsx

--- src/routes /kontakt.tsx
+++ src/routes /kontakt.tsx
@@ -1,1 +1,10 @@
+export default function Kontakt() {
+  return (
+    <main>
+      <h1>Kontakt</h1>
 
+      <p>E-Mail: info@firmenplus.com</p>
+      <p>Wir freuen uns auf Ihre Anfrage.</p>
+    </main>
+  )
+}
